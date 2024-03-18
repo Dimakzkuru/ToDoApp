9267e89eb878ba0e13f0e27965735673be404e39
Dimakzkuru <michaeldimakilingece@gmail.com>
2024-03-18 15:31:42 +0800

    Router Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ToDoFormLocalStorage } from "./Components/ToDoFormLocalStorage";
 
 function App() {
   return (
     <main className=" w-full h-screen flex items-start justify-center bg-gradient-to-r from-slate-700 to-gray-500">
-      <HashRouter basename="/ToDoApp">
-        <Routes>
-          <Route path="/" index element={<ToDoFormLocalStorage />} />
-        </Routes>
-      </HashRouter>
+      <Routes>
+        <Route path="/" index element={<ToDoFormLocalStorage />} />
+      </Routes>
     </main>
   );
 }
