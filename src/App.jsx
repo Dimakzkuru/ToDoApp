2830e1089d3bbb74dd6147ecec3bd73ea6b7e03b
Dimakzkuru <michaeldimakilingece@gmail.com>
2024-03-18 13:41:06 +0800

    remove route path  changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <main className=" w-full h-screen flex items-start justify-center bg-gradient-to-r from-slate-700 to-gray-500">
       <BrowserRouter basename="/ToDoApp">
         <Routes>
-          <Route path="/ToDoApp" index element={<ToDoFormLocalStorage />} />
+          <Route index element={<ToDoFormLocalStorage />} />
         </Routes>
       </BrowserRouter>
     </main>
