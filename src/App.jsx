796fd7e007e05e7dd23551cd93b4330e9201ce59
Dimakzkuru <michaeldimakilingece@gmail.com>
2024-03-18 14:08:45 +0800

    Route path Changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <main className=" w-full h-screen flex items-start justify-center bg-gradient-to-r from-slate-700 to-gray-500">
       <BrowserRouter basename="/ToDoApp">
         <Routes>
-          <Route index element={<ToDoFormLocalStorage />} />
+          <Route path="/" index element={<ToDoFormLocalStorage />} />
         </Routes>
       </BrowserRouter>
     </main>
