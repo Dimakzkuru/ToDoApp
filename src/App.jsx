1ebe9859ac07c171d89981888caf2fb75d010870
Dimakzkuru <michaeldimakilingece@gmail.com>
2024-03-18 14:34:37 +0800

    Prepare for Github Pages Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { ToDoFormLocalStorage } from "./Components/ToDoFormLocalStorage";
 
 function App() {
   return (
     <main className=" w-full h-screen flex items-start justify-center bg-gradient-to-r from-slate-700 to-gray-500">
-      <BrowserRouter basename="/ToDoApp">
+      <HashRouter basename="/ToDoApp">
         <Routes>
           <Route path="/" index element={<ToDoFormLocalStorage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </main>
   );
 }
